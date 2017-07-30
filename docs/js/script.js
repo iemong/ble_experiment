2dcfe57998699e35dbd5c89f6e66f16a574566a0
irie-shinnosuke <irie-shinnosuke@kayac.com>
2017-07-30 12:39:09 +0900

    localでは動かない気がするから上げる

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,19 +4,25 @@
 var SERVICE_UUID = '19B10000-E8F2-537E-4F6C-D104768A1214';
 var CHARACTERISTIC_UUID = '19B10001-E8F2-537E-4F6C-D104768A1214';
 
-navigator.bluetooth.requestDevice({
-    filters: [{
-        services: [SERVICE_UUID]
-    }]
-}).then(function (device) {
-    console.log('デバイスを検出しました');
-    return device.gatt.connect();
-}).then(function (server) {
-    return server.getPrimaryService(SERVICE_UUID);
-}).then(function (service) {
-    return service.getCharacteristic(CHARACTERISTIC_UUID);
-}).then(function (caracteristic) {
-    console.log("接続完了");
+var connectBluetooth = function connectBluetooth() {
+    navigator.bluetooth.requestDevice({
+        filters: [{
+            services: [SERVICE_UUID]
+        }]
+    }).then(function (device) {
+        console.log('デバイスを検出しました');
+        return device.gatt.connect();
+    }).then(function (server) {
+        return server.getPrimaryService(SERVICE_UUID);
+    }).then(function (service) {
+        return service.getCharacteristic(CHARACTERISTIC_UUID);
+    }).then(function (caracteristic) {
+        console.log("接続完了");
+    });
+};
+
+window.addEventListener('click', function () {
+    connectBluetooth();
 });
 
 },{}]},{},[1]);
