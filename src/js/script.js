e6bc25efe7ced1bebc1bc2590056584f0d9d6755
irie-shinnosuke <irie-shinnosuke@kayac.com>
2017-07-30 12:17:34 +0900

    スターターキット追加

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+const SERVICE_UUID = '19B10000-E8F2-537E-4F6C-D104768A1214';
+const CHARACTERISTIC_UUID = '19B10001-E8F2-537E-4F6C-D104768A1214';
+
+navigator.bluetooth.requestDevice({
+    filters: [{
+        services: [
+            SERVICE_UUID
+        ]
+    }]
+}).then(device => {
+    console.log('デバイスを検出しました');
+    return device.gatt.connect();
+}).then(server => {
+    return server.getPrimaryService(SERVICE_UUID);
+}).then(service => {
+    return service.getCharacteristic(CHARACTERISTIC_UUID);
+}).then(caracteristic => {
+    console.log("接続完了");
+});
